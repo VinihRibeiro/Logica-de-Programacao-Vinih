8ec01101621e699c4d62b4db244ab0d1e01c0237
Vinicius Ribeiro <126876848+VinihRibeiro@users.noreply.github.com>
2023-08-13 20:12:22 -0300

    Add files via upload

---
+++ Array/Array_for.js
@@ -0,0 +1,29 @@
+// Pode-se usar um loop for para percorrer os elementos de um array em JavaScript. Aqui está um exemplo simples de como usar o loop for para percorrer um array e imprimir seus elementos.
+
+//Exemplo:
+
+const number = [1,2,3,4,5,6];
+
+for(const numeros of number){
+    console.log(numeros);
+}
+
+// Saída: 1
+//        2
+//        3
+//        4
+//        5
+//        6
+
+//Outro exemplo que pode se usar For em um Array:
+
+for( let i = 0; i < number.length; i++ ){
+    console.log(number[i]);
+}
+
+// Saída: 1
+//        2
+//        3
+//        4 
+//        5
+//        6 
---
+++ Array/Array_push.js
@@ -0,0 +1,13 @@
+// O método push() é usado para adicionar um ou mais elementos ao final de um array. Ele modifica o array original, adicionando os novos elementos como elementos adicionais no final.
+
+// Aqui está um exemplo básico de como usar o push( ):
+
+let array = [1,2,3];
+console.log(array); // Saída: [1,2,3]
+
+array.push(4);
+console.log(array); //Saída: [1,2,3,4]
+
+array.push(5,6);
+console.log(array); // Saída: [1,2,3,4,5,6]
+
